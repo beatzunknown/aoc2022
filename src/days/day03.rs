59fa0fc1c2176fea5687a866c9e43a0301807fbc
Andrew Timkov <andrew.timkov@gmail.com>
2022-12-02 19:25:30 +1100

    Prepare for day 03

---
+++ src/days/day03.rs
@@ -0,0 +1,13 @@
+use aoc2022_lib::{utils, Day, Part};
+
+#[derive(Default)]
+pub struct Day03 {}
+
+impl<'a> Day<'a> for Day03 {
+    fn solve(&mut self, _input: &'a str, part: Part) -> String {
+        if part.is_first() {
+            return String::new();
+        }
+        String::new()
+    }
+}
