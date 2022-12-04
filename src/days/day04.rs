f5a3204923feb5b823b17d2ce32ace07a8ec8fc9
Andrew Timkov <andrew.timkov@gmail.com>
2022-12-04 16:44:20 +1100

    Finished day 04

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,13 +1,27 @@
-use aoc2022_lib::{utils, Day, Part};
+use aoc2022_lib::{Day, Part};
+use text_io::scan;
 
 #[derive(Default)]
 pub struct Day04 {}
 
 impl<'a> Day<'a> for Day04 {
     fn solve(&mut self, input: &'a str, part: Part) -> String {
-        if part.is_first() {
-            return String::new();
-        }
-        String::new()
+        input
+            .lines()
+            .map(|l| {
+                let (a, b, c, d): (u32, u32, u32, u32);
+                scan!(l.bytes() => "{}-{},{}-{}", a, b, c, d);
+                if part.is_first() {
+                    // 1st fully in 2nd or vice versa
+                    ((a <= c && b >= d) || (c <= a && d >= b)) as u32
+                } else {
+                    // 1st range starts in 2nd or vice versa
+                    ((a <= d) && (a >= c) || ((c <= b) && (c >= a))) as u32
+                }
+            })
+            .collect::<Vec<u32>>()
+            .iter()
+            .sum::<u32>()
+            .to_string()
     }
 }
