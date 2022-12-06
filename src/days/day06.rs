60bae279dc8c06d38f4a04e627369c7bea64a64f
Andrew Timkov <andrew.timkov@gmail.com>
2022-12-06 18:22:37 +1100

    Finished day 06

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,14 +1,21 @@
-use aoc2022_lib::{utils, Day, Part};
-use text_io::scan;
+use aoc2022_lib::{Day, Part};
+use std::collections::HashSet;
 
 #[derive(Default)]
 pub struct Day06 {}
 
 impl<'a> Day<'a> for Day06 {
-    fn solve(&mut self, _input: &'a str, part: Part) -> String {
-        if part.is_first() {
-            return String::new();
+    fn solve(&mut self, input: &'a str, part: Part) -> String {
+        let chars = input.trim().as_bytes();
+        let n = match part {
+            Part::One => 4,
+            Part::Two => 14,
+        };
+
+        let mut i = n;
+        while HashSet::<&u8>::from_iter(chars[(i - n)..i].iter()).len() < n {
+            i += 1;
         }
-        String::new()
+        i.to_string()
     }
 }
